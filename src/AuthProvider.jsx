b4258775e951fda6975fde05f3bbf6e0a43d974d
brodrickgeorge <brodrick.george@yahoo.com>
2021-07-31 00:01:05 -0500

    fixed log in

--- src/AuthProvider.jsx
+++ src/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { AsyncStorage } from "@react-native-async-storage/async-storage";
+import { AsyncStorage } from "react-native";
 
 export const AuthContext = React.createContext({});
 
