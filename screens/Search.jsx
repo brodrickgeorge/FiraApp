601778401b079a0c8f4b3f4676a5e34a4f8f4050
brodrickgeorge <brodrick.george@yahoo.com>
2021-08-01 23:20:02 -0500

    added home and search tabs

---
+++ screens/Search.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { globalStyles } from "../styles/Global";
+
+export default function Search() {
+  return (
+    <View style={globalStyles.centerItem}>
+      <Text>Search Screen</Text>
+    </View>
+  );
+}
