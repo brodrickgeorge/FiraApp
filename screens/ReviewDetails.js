693c4bc16445be4881af2403cd100fd1d9783c46
brodrickgeorge <brodrick.george@yahoo.com>
2021-08-03 00:19:37 -0500

    routing to review works

--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -4,19 +4,13 @@ import { View, Text } from "react-native";
 import { SafeAreaView } from "react-navigation";
 import { globalStyles } from "../styles/Global";
 
-export default function ReviewDetails({ navigation }) {
+export default function ReviewDetails({ route }) {
   return (
     <SafeAreaView style={globalStyles.containerSA}>
       <View style={globalStyles.container}>
-        <Text style={globalStyles.titleText}>
-          {navigation.getParam("title")}
-        </Text>
-        <Text style={globalStyles.titleText}>
-          {navigation.getParam("body")}
-        </Text>
-        <Text style={globalStyles.titleText}>
-          {navigation.getParam("rating")}
-        </Text>
+        <Text style={globalStyles.titleText}>{route.params.title}</Text>
+        <Text style={globalStyles.titleText}>{route.params.body}</Text>
+        <Text style={globalStyles.titleText}>{route.params.rating}</Text>
       </View>
     </SafeAreaView>
   );
