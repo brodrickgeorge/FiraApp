bf27117ac06cb53f2c5ab9868c11b579878760a8
brodrickgeorge <brodrick.george@yahoo.com>
2021-08-06 23:14:47 -0500

    added edit and done button

--- screens/Edit.jsx
+++ screens/Edit.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Text, View } from "react-native";
 import { globalStyles } from "../styles/Global";
 
-export default function Edit({ route }) {
+function apiCall(x) {
+  return x;
+}
+
+export default function Edit({ route, navigation }) {
+  const [formState] = useState();
+
+  const submit = useRef();
+
+  submit.current = () => {
+    //api call with new form state
+    apiCall(formState);
+    navigation.goBack();
+  };
+
+  useEffect(() => {
+    navigation.setParams({ submit });
+  }, []);
+
   return (
     <View style={globalStyles.centerItem}>
       <Text>Editing... {route.params.title} </Text>
